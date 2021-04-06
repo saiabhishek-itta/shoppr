56020a5276d161744a75635d9250b1a763459bcd
Sai Abhishek <abishekitta@gmail.com>
2021-04-06 23:20:37 +0530

    seller_signup_add2

--- seller_register.php
+++ seller_register.php
@@ -1,4 +1,38 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "shoppr";
 
-echo("hello bro!");
+$sell_name=$_POST['sell_name'];
+$sell_shopname=$_POST['sell_shopname'];
+$sell_mailid=$_POST['sell_mailid'];
+$sell_password=$_POST['sell_password'];
+$sell_address=$_POST['sell_address'];
+$sell_mobileno1=$_POST['sell_mobileno1'];
+$sell_mobileno2=$_POST['sell_mobileno2'];
+$sell_landline=$_POST['sell_landline'];
+
+
+
+
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO sellerdata (sell_name,sell_shopname,sell_mailid,sell_password,sell_address,sell_mobileno1,sell_mobileno2,sell_landline)
+VALUES ('$sell_name','$sell_shopname','$sell_mailid','$sell_password','$sell_address','$sell_mobileno1','$sell_mobileno2','$sell_landline')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "<h1>You are Successfully Registered as a seller</h1>";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
 ?>
