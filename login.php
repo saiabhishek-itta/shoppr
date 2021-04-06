45b6846cc890e330e068a33d0a1b5d8d62b37464
Sai Abhishek <62805698+i-sai-abhishek@users.noreply.github.com>
2021-04-06 23:41:42 +0530

    Update login.php

--- login.php
+++ login.php
@@ -16,20 +16,33 @@ $email = $_POST['email'];
 $password = $_POST['password']; 
 $accounttype = $_POST['accounttype'];
 if($accounttype == "seller") {
+  
     $sql = "SELECT * FROM sellerdata WHERE sell_mailid = '$email' and sell_password = '$password'";
-}
-else {
-    $sql = "SELECT * FROM customerdata WHERE cust_mailid = '$email' and cust_password = '$password'";
-}
-$result = $conn->query($sql);
-
-
-if($result->num_rows > 0) {
+  $result = $conn->query($sql);
+  if($result->num_rows > 0) {
     $_SESSION['userlogin'] = $email;
     header("location: http://localhost/shoppr/home.html");
 }
 else {
     echo "Your email or password is invalid";
 }
+  
+  
+}
+else {
+  
+    $sql = "SELECT * FROM customerdata WHERE cust_mailid = '$email' and cust_password = '$password'";
+  $result = $conn->query($sql);
+  if($result->num_rows > 0) {
+    $_SESSION['userlogin'] = $email;
+    header("location: http://localhost/shoppr/home.html");
+                  }
+  
+else {  echo "Your email or password is invalid";  }
+  
+  
+  
+}
+
 mysqli_close($conn);
 ?>
