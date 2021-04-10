7db8e5fd0be57c6bf153229ae7e336243f018277
Sai Abhishek <abishekitta@gmail.com>
2021-04-11 00:42:32 +0530

    Update login.php

--- login.php
+++ login.php
@@ -15,13 +15,14 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
-}
+} 
 
 session_start();
 $email = $_POST['email'];
-$password = $_POST['password'];
+$password = $_POST['password']; 
 $accounttype = $_POST['accounttype'];
 if($accounttype == "seller") {
+  
     $sql = "SELECT * FROM sellerdata WHERE sell_mailid = '$email' and sell_password = '$password'";
   $result = $conn->query($sql);
   if($result->num_rows > 0) {
@@ -30,23 +31,13 @@ if($accounttype == "seller") {
     header("location: http://localhost/shoppr/seller_home.php");
 }
 else {
-    $sql = "SELECT * FROM customerdata WHERE cust_mailid = '$email' and cust_password = '$password'";
+    echo "Your email or password is invalid";
 }
-$result = $conn->query($sql);
-
-if($result->num_rows > 0) {
-    $_SESSION['userlogin'] = $email;
-    $_SESSION['account']
-
-    if($accounttype == "seller") {
-        header("location: http://localhost/shoppr/sellerhome.html");
-    }
-    else {
-        header("location: http://localhost/shoppr/customerhome.html");
-    }
+  
+  
 }
 else {
-
+  
     $sql = "SELECT * FROM customerdata WHERE cust_mailid = '$email' and cust_password = '$password'";
   $result = $conn->query($sql);
   if($result->num_rows > 0) {
@@ -54,11 +45,12 @@ else {
     $_SESSION['account_type'] = $accounttype;
     header("location: http://localhost/shoppr/customer_home.php");
                   }
-
+  
 else {  echo "Your email or password is invalid";  }
-
-
-
+  
+  
+  
 }
+
 mysqli_close($conn);
 ?>
